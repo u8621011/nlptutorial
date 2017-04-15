31b615ae2dcb4abff33718314184ecd7c510fa57
Ted Chen <shapable.ted@gmail.com>
2017-04-15 12:32:31 +0800

    implementing a01 entropy

--- a01.py
+++ a01.py
@@ -21,22 +21,64 @@ def getTokenDict (fin):
 
     return dictTokens
 
-def doTrain(ifile, ofile):
+def TrainModel(ifile, ofile):
     fin = open(ifile, 'r', encoding='utf8')
 
     d = getTokenDict(fin)
     total = sum(d.values())
-    p = {k: v/float(total) for k, v in d.items()}
+    model = {k: v/float(total) for k, v in d.items()}
 
     fout = open(ofile, 'w', encoding='utf8')
 
-    for k, v in p.items():
+    for k, v in model.items():
         outString = "%s %s" % (k, v)
         print(outString)
         fout.write("%s\n" % outString)
 
     fin.close()
     fout.close()
+
+    return model
+
+# return properbility of each lin
+def DoEntropy (model, lines):
+    lamda1 = 0.95
+    lamdaUnk = 1 - lamda1
+    v = 1000000
+
+    # statistic variables
+    totalTokenCount = 0
+    unknownCount = 0
+    dictTokenProps = dict()
+
+    # calc token total count
+    unkProp = lamdaUnk / v
+    for curLine in lines:
+        line = curLine.strip()
+        tokens = line.split()
+        # include end of sentance symbo </s>
+        tokens.append('</s>')
+
+        # calc properbility of each token
+        for curToken in tokens:
+            totalTokenCount += 1
+            tokenProp = unkProp
+            if curToken in model:
+                tokenProp += model[curToken] * lamda1
+            else:
+                unknownCount += 1
+
+            if curToken in dictTokenProps:
+                dictTokenProps[curToken] += tokenProp
+            else:
+                dictTokenProps[curToken] = tokenProp
+
+
+    return {
+        'TotalCount': totalTokenCount,
+        'UnknownCount': unknownCount,
+        'LogLikes': logLikes
+    }
 
 if len(sys.argv) < 4:
     print("""execute format:
@@ -50,7 +92,7 @@ ifile = sys.argv[2]
 ofile = sys.argv[3]
 
 if mode == 'train':
-    doTrain(ifile, ofile)
+    TrainModel(ifile, ofile)
 elif mode == 'test':
     doTest()
 else:
