2bf8dd79bd0bba36df2b03190c1894f05eb43b7f
Ted Chen <shapable.ted@gmail.com>
2017-04-20 21:03:30 +0800

    complete graph building and viterbi algorithm

--- a03.py
+++ a03.py
@@ -1,30 +1,36 @@
 import helper
 import sys
+import math
 from collections import namedtuple
 
 def buildGraph (model, sentance):
+    print ('Building graph for sentance: {}'.format(sentance))
+
     slen = len(sentance)
 
-    graph = []
+    graph = [None] * slen
     for i in range(slen):
-        edges = []
+        # add first edge (i, i+1)
         # add edge if the partial word can be found in dict
-        for j in range(1, slen):
+        for j in range(i+1, slen):
             subString = sentance[i:j]
+            print('substring[{}:{}]: {}'.format(i, j, subString))
             if subString in model:
                 p = model[subString]
-                edge = Edge(i, j, p)
-                edges.append(edge)
-                print('appending eage : {}'.format(edge))
+                score = -math.log(p)
+                edge = Edge(i, j, score)
 
-        graph.append(edges)
+                curEdges = graph[j]
+                if curEdges == None:
+                    graph[j] = [edge]
+                else:
+                    curEdges.append(edge)
 
+    print('Graph built: {}'.format(graph))
 
     return graph
                 
-    
-
-def doSegmentation (model, line):
+def doViterbiSegmentation (model, line):
     sentance = line.strip()
     slen = len(sentance)
 
@@ -33,8 +39,37 @@ def doSegmentation (model, line):
 
     graph = buildGraph(model, line)
 
-    print(graph)
-    
+    # forward steps
+    nodeBestScore[0] = 0
+    for i in range(1, slen+1):
+        curNodeIncomingEdges = graph[i]
+        for curEdge in curNodeIncomingEdges:
+            if nodeBestScore[i] == None:
+                nodeBestScore[i] = nodeBestScore[curEdge.S] + curEdge.Score
+                nodeBestEdge[i] = curEdge
+            else:
+                curScore = nodeBestScore[curEdge.S] + curEdge.Score
+                if curScore < nodeBestScore[i]:
+                     nodeBestScore[i] = curScore
+                     nodeBestEdge[i] = curEdge
+
+    print('Best scores: {}'.format(nodeBestScore))
+    print('Best edges: {}'.format(nodeBestEdge))
+
+    # backward steps
+    bestPath = []
+    curBestEdge = nodeBestEdge[slen]
+    while curBestEdge != None:
+        bestPath.append(curBestEdge)
+        curBestEdge = nodeBestEdge[curBestEdge.S]
+
+    print('Best Path: {}'.format(bestPath))
+    tokens = []
+    for curEdge in reversed(bestPath):
+        sub = sentance[curEdge.S:curEdge.E]
+        tokens.append(sub)
+
+    print('Segment Result: {}'.format(' '.join(tokens)))
 
 def SegmentFile(model, segFile, outFile):
     sf = open(segFile, 'r', encoding='utf8')
@@ -42,7 +77,7 @@ def SegmentFile(model, segFile, outFile):
 
     lines = sf.readlines()
     for curLine in lines:
-        tokens = doSegmentation (model, curLine)
+        tokens = doViterbiSegmentation (model, curLine)
         #of.write(" ".join(tokens))
 
     sf.close()
@@ -72,7 +107,7 @@ elif mode == 'seg':
     file3 = sys.argv[4]
     model = helper.LoadModel(file1)
 
-    Edge = namedtuple('Edge', ['S', 'E', 'P'])
+    Edge = namedtuple('Edge', ['S', 'E', 'Score'])
     SegmentFile(model, file2, file3)
 else:
     print('Unknown execution mode')
